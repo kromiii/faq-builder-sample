9cc624522b8046875e998b643e3d0bf9327f1f50
Hiroyuki Kuromiya <15026387+kromiii@users.noreply.github.com>
2023-11-18 11:27:37 +0900

    一旦完成

---
+++ extract_faq.py
@@ -0,0 +1,71 @@
+import time
+import json
+
+from openai import OpenAI
+
+client = OpenAI()
+
+custom_function = {
+    "name": "generate_faq",
+    "description": "Generates a FAQ from a manual.",
+    "parameters": {
+        "type": "object",
+        "properties": {
+            "faq_items": {
+                "type": "array",
+                "description": "A list of FAQ items.",
+                "items": {
+                    "type": "object",
+                    "properties": {
+                        "question": {
+                            "type": "string",
+                            "description": "The question."
+                        },
+                        "answer": {
+                            "type": "string",
+                            "description": "The answer."
+                        }
+                    },
+                    "required": ["question", "answer"]
+                }
+            }
+        },
+        "required": ["faq_items"]
+    }
+}
+
+def extract_faq_with_openai(pdf_file):
+    file = client.files.create(
+        file=pdf_file,
+        purpose='assistants'
+    )
+    assistant = client.beta.assistants.create(
+        name="FAQ Builder",
+        instructions="PDFのマニュアルを受け取って FAQ サイトを生成してください。FAQは日本語で生成してください。",
+        tools=[
+            {"type": "retrieval"},
+            {"type": "function", "function": custom_function}
+        ],
+        model="gpt-4-1106-preview",
+        file_ids=[file.id]
+    )
+    thread = client.beta.threads.create()
+    message = client.beta.threads.messages.create(
+        thread_id=thread.id,
+        role="user",
+        content="このマニュアルからFAQを生成してください。",
+    )
+    run = client.beta.threads.runs.create(
+        thread_id=thread.id,
+        assistant_id=assistant.id,
+    )
+    while run.status != "requires_action":
+        run = client.beta.threads.runs.retrieve(thread_id=thread.id, run_id=run.id)
+        print("run.status:", run.status)
+        time.sleep(5)
+    
+    result = run.required_action.submit_tool_outputs.tool_calls[0].function.arguments
+    print("result:", result)
+    result = json.loads(result)
+
+    return result["faq_items"]
--- faq-builder.py
+++ faq-builder.py
@@ -1,6 +1,7 @@
 import streamlit as st
 import streamlit.components.v1 as components
 
+from extract_faq import extract_faq_with_openai
 from generate_html import generate_html
 
 def main():
@@ -8,15 +9,21 @@ def main():
     
     uploaded_file = st.file_uploader("Upload your PDF", type="pdf")
     if uploaded_file is not None:
-        # faq = generate_faq_with_openai(uploaded_file)
-        # st.write(faq)
-
-        faq_items = {
-            "質問1": "回答1",
-            "質問2": "回答2",
-            "質問3": "回答3",
-            # 他の質問と回答
-        }
+        faq_items = extract_faq_with_openai(uploaded_file)
+        # faq_items = [
+        #     {
+        #         "question": "How do I change my password?",
+        #         "answer": "You can change your password by clicking on the Settings button in the top right corner of the screen."
+        #     },
+        #     {
+        #         "question": "How do I change my email address?",
+        #         "answer": "You can change your email address by clicking on the Settings button in the top right corner of the screen."
+        #     },
+        #     {
+        #         "question": "How do I change my profile picture?",
+        #         "answer": "You can change your profile picture by clicking on the Settings button in the top right corner of the screen."
+        #     },
+        # ]
         html_content = generate_html(faq_items=faq_items)
 
         components.html(html_content, height=800)
