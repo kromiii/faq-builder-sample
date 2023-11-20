625a65b86080efafaab6bc0117e3633d6dd39606
Hiroyuki Kuromiya <15026387+kromiii@users.noreply.github.com>
2023-11-21 08:41:36 +0900

    既存の assistant を使う

--- extract_faq.py
+++ extract_faq.py
@@ -4,56 +4,20 @@ import json
 from openai import OpenAI
 
 client = OpenAI()
-
-custom_function = {
-    "name": "generate_faq",
-    "description": "Generates a FAQ from a manual.",
-    "parameters": {
-        "type": "object",
-        "properties": {
-            "faq_items": {
-                "type": "array",
-                "description": "A list of FAQ items.",
-                "items": {
-                    "type": "object",
-                    "properties": {
-                        "question": {
-                            "type": "string",
-                            "description": "The question."
-                        },
-                        "answer": {
-                            "type": "string",
-                            "description": "The answer."
-                        }
-                    },
-                    "required": ["question", "answer"]
-                }
-            }
-        },
-        "required": ["faq_items"]
-    }
-}
+assistant_id = "asst_4kiIu1akNbn1fnWFzL3slVxa"
 
 def extract_faq_with_openai(pdf_file):
     file = client.files.create(
         file=pdf_file,
         purpose='assistants'
     )
-    assistant = client.beta.assistants.create(
-        name="FAQ Builder",
-        instructions="PDFのマニュアルを受け取って FAQ サイトを生成してください。FAQは日本語で生成してください。",
-        tools=[
-            {"type": "retrieval"},
-            {"type": "function", "function": custom_function}
-        ],
-        model="gpt-4-1106-preview",
-        file_ids=[file.id]
-    )
+    assistant = client.beta.assistants.retrieve(assistant_id)
     thread = client.beta.threads.create()
     message = client.beta.threads.messages.create(
         thread_id=thread.id,
         role="user",
         content="このマニュアルからFAQを生成してください。",
+        file_ids=[file.id]
     )
     run = client.beta.threads.runs.create(
         thread_id=thread.id,
@@ -63,7 +27,7 @@ def extract_faq_with_openai(pdf_file):
         run = client.beta.threads.runs.retrieve(thread_id=thread.id, run_id=run.id)
         print("run.status:", run.status)
         time.sleep(5)
-    
+
     result = run.required_action.submit_tool_outputs.tool_calls[0].function.arguments
     result = json.loads(result)
 
